631fe7b8cf9e4d082398b01be191e74e63de5320
Kim Leppänen <kim@vaadin.com>
2019-08-09 15:54:00 +0300

    Implemented hashed password

--- src/main/java/org/vaadin/hackme/spring/users/UserRepository.java
+++ src/main/java/org/vaadin/hackme/spring/users/UserRepository.java
@@ -1,10 +1,14 @@
 package org.vaadin.hackme.spring.users;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -13,20 +17,23 @@ import org.springframework.stereotype.Component;
 public class UserRepository {
 
 	@Autowired
-	JdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
+
+	@Value("${hackme.pwdsalt}")
+	private String salt;
 
 	public User getUser(String username, String password) {
-		String query = "select id, username, password from users where username = '"
-				+ username + "' and password = '" + password + "'";
+		String query = "select id, username, password from users where username = '" + username + "' and password = '"
+				+ hashPassword(password) + "'";
 		User user = this.jdbcTemplate.queryForObject(query, new Object[] {}, new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getLong("id"));
-						user.setUsername(rs.getString("username"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getLong("id"));
+				user.setUsername(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		});
 
 		if (user != null) {
 			List<Role> roles = this.jdbcTemplate.query("select role from role_mapping where user_id=?",
@@ -40,6 +47,29 @@ public class UserRepository {
 		}
 
 		return user;
+	}
+
+	private String hashPassword(String clearTextPassword) {
+		MessageDigest digest;
+		try {
+			String t = clearTextPassword + salt;
+			digest = MessageDigest.getInstance("SHA-256");
+			byte[] encodedhash = digest.digest(t.getBytes(StandardCharsets.UTF_8));
+			return bytesToHex(encodedhash);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	private static String bytesToHex(byte[] hash) {
+	    StringBuffer hexString = new StringBuffer();
+	    for (int i = 0; i < hash.length; i++) {
+	    String hex = Integer.toHexString(0xff & hash[i]);
+	    if(hex.length() == 1) hexString.append('0');
+	        hexString.append(hex);
+	    }
+	    return hexString.toString();
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into news (id, article) values (1, 'Welcome to the <b>Vaadin HACKathon</b>!');
 
 -- Password masking not implemented in first iteration
-insert into users (id, username, password) values (1, 'admin','admin');
+insert into users (id, username, password) values (1, 'admin','e46c51f28b92c31db4c9dee3c3d08cbf1dc1c857465468d9947595b598b3bbc6');
 
 insert into role_mapping (user_id, role) values (1, 'NORMAL'), (1,'ADMIN');
